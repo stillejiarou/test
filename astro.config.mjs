c50c8b3f7e72d63f0386fded5ff068eece04d3b7
Stille Jia-Rou <@stillejiarou.proton.me>
2026-03-16 21:02:49 +0100

    + astro: install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
-// https://astro.build/config
 export default defineConfig({
+  integrations: [preact()],
   experimental: {
     rustCompiler: true
   }
