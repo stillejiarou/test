d962a3a3a89c30813bd61e220639d38c8553defa
Stille Jia-Rou <@stillejiarou.proton.me>
2026-03-16 20:37:57 +0100

    + astro: global constants

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE_NAME = "Hello World";
+export const SITE_DESCRIPTION = "A personal blog about anything on Jia-Rou's mind.";
+export const SITE_URL = "https://stillejiarou.neocities.org";
+export const AUTHOR = "Jia-Rou";
+export const SOCIAL = {
+  github: "https://github.com/stillejiarou",
+  neocities: "https://neocities,org/site/stillejiarou",
+};
+export const DEFAULT_OG_IMAGE = "/preview.png";
+export const THEME_COLOR_LIGHT = "#ffffff";
+export const THEME_COLOR_DARK = "#000000";
+export const POSTS_PER_PAGE = 10;
+export const DATE_FORMAT = "YYYY.MM.DD";
