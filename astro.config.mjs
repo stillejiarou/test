cb36af8a9f230b8ef3ac54ccc9e031b6b19b733d
Stille Jia-Rou <@stillejiarou.proton.me>
2026-03-17 09:26:10 +0100

    - astro: uninstall preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [],
   experimental: {
     rustCompiler: true
   }
